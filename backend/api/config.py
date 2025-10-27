7f74e16d0adb918ccf9a40e5d8f8b0e8dd10e5b8
Mudit Pandey <Muditpanday17@gmail.com>
2025-10-27 12:08:37 +0530

    Update config.py

--- backend/api/config.py
+++ backend/api/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    HOST: str = "127.0.0.1"
+    HOST: str = "0.0.0.0"
     PORT: int = 8000
     RELOAD: bool = True
 
